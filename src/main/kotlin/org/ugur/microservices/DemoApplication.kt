f1f482d2bb760e627b802a06d0f0e887f80ef627
Uğur Artun <ugur.artun@sahibinden.com>
2019-12-25 13:17:06 +0300

    get method for user

--- src/main/kotlin/org/ugur/microservices/DemoApplication.kt
+++ src/main/kotlin/org/ugur/microservices/DemoApplication.kt
@@ -2,9 +2,20 @@ package org.ugur.microservices
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.ResponseBody
 
 @SpringBootApplication
 class DemoApplication
+
+@Controller
+class FirstController {
+
+    @GetMapping(value = ["/user"])
+    @ResponseBody
+    fun hello() = "hello user"
+}
 
 fun main(args: Array<String>) {
     runApplication<DemoApplication>(*args)
