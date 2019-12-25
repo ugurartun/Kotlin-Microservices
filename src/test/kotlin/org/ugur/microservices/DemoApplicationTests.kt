187b7c3ba0e44a4969485ed6a50c0152331a10ac
Uğur Artun <ugur.artun@sahibinden.com>
2019-12-25 13:09:48 +0300

    Initial commit

---
+++ src/test/kotlin/org/ugur/microservices/DemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.ugur.microservices
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
