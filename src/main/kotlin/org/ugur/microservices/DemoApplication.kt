15b0374afa7b25dc9eeefbda569a988602fd82fb
Uğur Artun <ugur.artun@sahibinden.com>
2019-12-25 13:25:38 +0300

    modify controller

--- src/main/kotlin/org/ugur/microservices/DemoApplication.kt
+++ src/main/kotlin/org/ugur/microservices/DemoApplication.kt
@@ -4,17 +4,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.ResponseBody
 
 @SpringBootApplication
 class DemoApplication
 
 @Controller
-class FirstController {
+class FirstController(val exampleService: ExampleService) {
 
-    @GetMapping(value = ["/user"])
+    @GetMapping(value = ["/user/{name}"])
     @ResponseBody
-    fun hello() = "hello user"
+    fun hello(@PathVariable name :String) = exampleService.getHello(name)
 }
 
 fun main(args: Array<String>) {
