0c138104aa5b5a3ccfa1d64c6bdbc4e4a14e7f4c
Uğur Artun <ugur.artun@sahibinden.com>
2019-12-27 16:24:34 +0300

    refactoring

--- src/main/kotlin/org/ugur/microservices/CustomerController.kt
+++ src/main/kotlin/org/ugur/microservices/CustomerController.kt
@@ -1,5 +1,6 @@
 package org.ugur.microservices
 
+import com.ugur.microservices.service.CustomerService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
