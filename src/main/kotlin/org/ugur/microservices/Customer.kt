b8db38628f3c99cda2815740481ba56bfae9eaba
Ugur Artun <artun.ugur@gmail.com>
2019-12-28 11:53:18 +0300

    jackson serialize

--- src/main/kotlin/org/ugur/microservices/Customer.kt
+++ src/main/kotlin/org/ugur/microservices/Customer.kt
@@ -1,3 +1,6 @@
 package org.ugur.microservices
 
-data class Customer (var id : Int=0, var name :  String="")
+import com.fasterxml.jackson.annotation.JsonInclude
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+data class Customer (var id : Int=0, var name :  String="", var telephone : Telephone? = null) 
---
+++ src/main/kotlin/org/ugur/microservices/Telephone.kt
@@ -0,0 +1,3 @@
+package org.ugur.microservices
+
+data class Telephone(var countryCode : String = "", var telephoneNumber: String = "")
