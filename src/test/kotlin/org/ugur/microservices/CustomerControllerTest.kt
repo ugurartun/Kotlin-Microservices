0fea0850502810c8f79dbba9fceb2b8e62be87a3
Ugur Artun <artun.ugur@gmail.com>
2020-01-01 23:14:43 +0300

    controller test

---
+++ src/test/kotlin/org/ugur/microservices/CustomerControllerTest.kt
@@ -0,0 +1,33 @@
+package org.ugur.microservices
+
+import org.junit.jupiter.api.Test
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.web.servlet.MockMvc
+
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers.print
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+internal class CustomerControllerTest {
+
+    @Autowired
+    lateinit var mockMvc: MockMvc
+
+    @Test
+    fun getByIdControllerTest() {
+        mockMvc.perform(get("/customer/1"))
+                .andExpect(status().isOk)
+                .andExpect(jsonPath("\$.id").value(1))
+                .andExpect(jsonPath("\$.name").value("Kotlin"))
+                .andDo(print())
+    }
+
+
+}
--- src/test/kotlin/org/ugur/microservices/CustomerServiceTest.kt
+++ src/test/kotlin/org/ugur/microservices/CustomerServiceTest.kt
@@ -1,6 +1,5 @@
 package org.ugur.microservices
 
-import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 
 import org.junit.jupiter.api.Assertions.*
@@ -16,12 +15,18 @@ internal class CustomerServiceTest {
     @Test
     fun getCustomers() {
         val customer = customerService.getCustomer(1)
-        Assertions.assertEquals("Kotlin", customer?.name, "Customer 1 name is Kotlin")
+        assertEquals("Kotlin", customer?.name, "Customer 1 name is Kotlin")
     }
 
     @Test
     fun getAllCustomers() {
         val customers = customerService.getAllCustomers()
-        Assertions.assertEquals(3, customers.size)
+        assertEquals(3, customers.size)
+    }
+
+    @Test
+    fun getCustomersAbsentOnSystem() {
+        val customer = customerService.getCustomer(5)
+        assertEquals(null, customer?.name, "Customer 5 not found on system")
     }
 }
