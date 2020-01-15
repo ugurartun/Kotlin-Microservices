95c28dc6f1edc3425aeadb010fde1f5fb0230c75
Ugur Artun <artun.ugur@gmail.com>
2020-01-15 21:16:01 +0300

    adding repository layer

--- src/main/kotlin/org/ugur/microservices/DemoApplication.kt
+++ src/main/kotlin/org/ugur/microservices/DemoApplication.kt
@@ -1,15 +1,23 @@
 package org.ugur.microservices
 
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
-import java.util.concurrent.ConcurrentHashMap
+import org.ugur.microservices.repository.CustomerRepository
 
 @SpringBootApplication
 class DemoApplication  {
 
 }
 
+@Bean
+fun init(repository: CustomerRepository) = CommandLineRunner {
+    repository.save(Customer(1, "Bauer"))
+    repository.save(Customer(2, "O'Brian"))
+    repository.save(Customer(3, "Bauer"))
+    repository.save(Customer(4, "Palmer"))
+}
 
 fun main(args: Array<String>) {
     runApplication<DemoApplication>(*args)
