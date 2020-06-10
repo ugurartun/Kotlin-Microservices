62fb74b2f415cec7c2f6b73ab12847f5236656d2
Uğur Artun <ugur.artun@sahibinden.com>
2020-06-10 22:26:07 +0300

    refactor

--- src/main/kotlin/org/ugur/microservices/CustomerController.kt
+++ src/main/kotlin/org/ugur/microservices/CustomerController.kt
@@ -5,28 +5,29 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import org.ugur.microservices.exception.CustomerNotFoundException
+import org.ugur.microservices.model.Customer
 
 @RestController
-class CustomerController {    
+class CustomerController {
     @Autowired
     private lateinit var customerService: CustomerService
 
     @GetMapping("/customer/{id}")
-    fun getCustomer(@PathVariable id : Int) : ResponseEntity<Customer> {
+    fun getCustomer(@PathVariable id: Int): ResponseEntity<Customer> {
         val customer = customerService.getCustomer(id) ?: throw CustomerNotFoundException("customer '$id' not found")
         return ResponseEntity(customer, HttpStatus.OK)
     }
 
     @PostMapping("/customer")
-    fun createCustomer(@RequestBody customer: Customer) : ResponseEntity<Unit> {
+    fun createCustomer(@RequestBody customer: Customer): ResponseEntity<Unit> {
         customerService.createCustomer(customer)
         return ResponseEntity(Unit, HttpStatus.CREATED)
     }
 
     @DeleteMapping("/customer/{id}")
-    fun deleteCustomer(@PathVariable id : Int) : ResponseEntity<Unit> {
+    fun deleteCustomer(@PathVariable id: Int): ResponseEntity<Unit> {
         var status = HttpStatus.NOT_FOUND
-        if(customerService.getCustomer(id) != null)  {
+        if (customerService.getCustomer(id) != null) {
             customerService.deleteCustomer(id)
             status = HttpStatus.OK
         }
@@ -34,9 +35,9 @@ class CustomerController {
     }
 
     @PutMapping("/customer/{id}")
-    fun updateCustomer(@PathVariable id : Int, @RequestBody customer: Customer) : ResponseEntity<Unit?> {
+    fun updateCustomer(@PathVariable id: Int, @RequestBody customer: Customer): ResponseEntity<Unit?> {
         var status = HttpStatus.NOT_FOUND
-        if(customerService.getCustomer(id) != null)  {
+        if (customerService.getCustomer(id) != null) {
             customerService.updateCustomer(id, customer)
             status = HttpStatus.ACCEPTED
         }
@@ -44,7 +45,7 @@ class CustomerController {
     }
 
     @GetMapping("/customers")
-    fun getCustomers(@PathVariable id : Int) : List<Customer> {
+    fun getCustomers(@PathVariable id: Int): List<Customer> {
         return customerService.getAllCustomers()
     }
 
--- src/main/kotlin/org/ugur/microservices/repository/CustomerRepository.kt
+++ src/main/kotlin/org/ugur/microservices/repository/CustomerRepository.kt
@@ -1,7 +1,7 @@
 package org.ugur.microservices.repository
 
 import org.springframework.data.repository.CrudRepository
-import org.ugur.microservices.Customer
+import org.ugur.microservices.model.Customer
 
 interface CustomerRepository : CrudRepository<Customer, Long> {
     fun findByName(name: String): List<Customer>
